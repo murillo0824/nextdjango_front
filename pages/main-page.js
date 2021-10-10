78801276dd05f18805337361a1ba416594df76cb
Shoiti Tsuchiya <murillo.mild@gmail.com>
2021-10-10 12:29:04 +0900

    make blog list page

--- pages/main-page.js
+++ pages/main-page.js
@@ -1,6 +1,7 @@
 import Cookie from "universal-cookie";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 const cookie = new Cookie();
 
@@ -12,6 +13,18 @@ export default function MainPage() {
   };
   return (
     <Layout title="main page">
+      <div className="mb-12">
+        <Link href="/blog-page">
+          <a className="bg-indigo-500 mr-8 hover:bg-indigo-600 text-white px-4 py-12 rounded">
+            visit blog page by ssg + isr
+          </a>
+        </Link>
+        <Link href="/task-page">
+          <a className="bg-gray-500 mr-8 hover:bg-gray-600 text-white px-4 py-12 rounded">
+            visit blog page by ssg + isr
+          </a>
+        </Link>
+      </div>
       <svg
       onClick={logout}
       xmlns="http://www.w3.org/2000/svg"
